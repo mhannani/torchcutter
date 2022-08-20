e38c20e754a778cc3f1fbb20462321ad8d95f54c
mhannani <mohamed.hanani2019@gmail.com>
2022-08-20 19:32:13 +0100

    Firing !

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A logical, reasonably standardized, but flexible framework for training, evaluating, and deploying PyTorch models.',
+    author='mhannani',
+    license='MIT',
+)
