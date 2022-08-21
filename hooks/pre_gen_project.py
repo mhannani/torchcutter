a3dd31e35ffd262d86a973503426c6cb947e420e
mhannani <mohamed.hanani2019@gmail.com>
2022-08-21 01:33:57 +0100

    changes

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -5,9 +5,9 @@ def welcoming():
 ├───────────────────────────────────────────────────────────────────│
 │                                                                   │
 │ Torchcutter is project template for training, evaluating,         │
-│ monitoring and deploying PyTorch model to overcome the overhead   │
-│ of setting up a  PyTorch project and instead focus on             │
-│ system/product quality.                                           │
+│ monitoring and deploying PyTorch models to overcome the overhead  │
+│ of setting up a PyTorch project, and instead focus on system      │
+│ and product quality.                                              │
 │                                                                   │
 ├───────────────────────────────────────────────────────────────────│
 │ - Repository: https://github.com/mhannani/torchcutter             │
